dd2565d2439c53bcbf6a1e5a19a9f1b4d9476b92
arperuma <arperuma@ncsu.edu>
2025-07-08 22:06:51 -0400

    Update routes for App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import { Home } from './pages/Home.js';
-import { Visitors } from "./pages/Visitors.js";
-import { Members } from "./pages/Members.js";
-import { User } from "./pages/User.js";
-import { AddVisitors } from "./pages/AddVisitors.js";
+import { Home } from '../client/pages/Home.js';
+import { Visitors } from "../client/pages/Visitors.js";
+import { Members } from "../client/pages/Members.js";
+import { User } from "../client/pages/User.js";
+import { AddVisitors } from "../client/pages/AddVisitors.js";
 
 function App() {
 
